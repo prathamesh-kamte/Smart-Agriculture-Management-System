1457c85d5cef4ae15d70676662d5e97e266b1ecc
prathamesh <prathameshkamte@gmail.com>
2026-04-29 23:25:08 +0530

    feat: implement MapStruct mappers for Crop, Expense, and User entities

---
+++ src/main/java/com/smartagri/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.smartagri.mapper;
+
+import com.smartagri.domain.dto.UserDto;
+import com.smartagri.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "password", ignore = true)
+    UserDto toDto(User user);
+
+    User toEntity(UserDto userDto);
+}
